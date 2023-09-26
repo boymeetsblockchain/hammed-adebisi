289520b9dc606e896a79da46f2178a9f84b0c7e0
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-09-26 06:47:23 +0100

    added new content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,28 @@ import Adebisi from '../assets/Adebisi.jpeg';
 
 function Hero() {
   return (
-    <div className="hero flex flex-col-reverse md:flex-row w-full mx-auto md:px-16 my-10 items-center">
+    <div className="hero flex flex-col-reverse md:flex-row w-full gap-2 mx-auto md:px-16 my-10 items-center">
       <div className="info flex flex-col justify-center md:w-1/2 md:mx-[100px] space-y-3 text-center md:text-left">
-        <h1 className='text-3xl md:text-4xl text-neutral-800 font-semibold'>
-          Hammed Adebisi
+        <h1 className='text-3xl md:text-4xl text-neutral-800 font-bold'>
+          Hammed I. Adebisi,PhD
         </h1>
-        <h2 className='text-2xl md:text-3xl text-gray-600'>
-          Medical Rehabilitationist and Clinical Researcher
+        <h2 className='text-2xl md:text-2xl text-gray-600'>
+          Neurological Physiotherapist, Exercise Physiologist,Public Health Specialist and Clinical Researcher
         </h2>
         <p className='text-gray-700'>
-          A Medical Rehabilitationist and Clinical Researcher is a healthcare professional with dual roles. As a Medical Rehabilitationist, they specialize in assessing and treating individuals who have experienced injuries, illnesses, or disabilities that affect their physical function and mobility. They use therapies and interventions to improve patients' quality of life and independence. Simultaneously, as a Clinical Researcher, they conduct scientific studies to enhance the effectiveness of rehabilitation treatments and interventions. They design research projects, collect and analyze data, and contribute to evidence-based practice in the field of medical rehabilitation, benefiting both individual patients and the broader healthcare community.
+          Physiotherapy is a health care profession concerned with human function and movement and maximizing physical potential. It is concerned with identifying and maximizing quality of life and movement potential within the spheres of promotion, prevention, treatment/intervention, habilitation and rehabilitation. It uses physical approaches to promote, maintain and restore physical, psychological and social well-being, considering variations in health status.  It is science-based, committed to extending, applying, evaluating and reviewing the evidence that underpins and informs its practice and delivery.  The exercise of clinical judgement and informed interpretation is at its core. Physiotherapy addresses the illnesses or injuries that limit a person's abilities to move and perform functional activities in their daily lives. Physiotherapists use an individual's history and physical examination to arrive at a diagnosis and establish a management plan and, when necessary, incorporate the results of laboratory and imaging studies like X-rays, CT-scan, or MRI findings. Electrodiagnostic testing (e.g., electromyograms and nerve conduction velocity testing) may also be used.
+
+        </p>
+        <p className="text-gray-700">
+          Exercise physiology is the physiology of physical exercise. It is one of the allied health professions, and involves the study of the acute responses and chronic adaptations to exercise. Exercise physiologists are the highest qualified exercise professionals and utilize education, lifestyle intervention and specific forms of exercise to rehabilitate and manage acute and chronic injuries and conditions. Exercise physiologists study the effect of exercise on pathology, and the mechanisms by which exercise can reduce or reverse disease progression.
+
+        </p>
+        <p className="text-gray-700">
+          A public health specialist is a health professional who researches diseases and illnesses and assists medical professionals and epidemiologists in detecting and preventing infectious diseases. They also discover ways to decrease health threats and promote healthy lifestyles through education and training. Public health specialists focus their careers on promoting healthy lifestyles for the public through research and analysis and also seek to discover ways to decrease health disparities across communities and populations. They also conduct environmental health surveys, provide health education and training.
+
+        </p>
+        <p className="text-gray-700">
+          Clinical research is a branch of healthcare science that determines the safety and effectiveness of medications, devices, diagnostic products and treatment regimens intended for human use. These may be used for prevention, treatment, diagnosis or for relieving symptoms of a disease. A clinical researcher oversees the development and implementation of clinical research projects to assess the safety and effectiveness of various medications, diagnostic products and medical devices
         </p>
       <div className='flex md:justify-start justify-center'>
       <a href="#7" className='bg-[#ff7999] text-white py-2 px-4 rounded-md inline-block mt-4 hover:bg-red-600 transition duration-300'>
@@ -20,11 +32,11 @@ function Hero() {
         </a>
       </div>
       </div>
-      <div className="image md:w-1/2 flex justify-center">
+      <div className="image md:w-1/2 flex md:justify-end">
   <img
     src={Adebisi}
     alt="Hammed Adebisi"
-    className="h-64 w-64 lg:h-128 lg:w-128 rounded-full"
+    className="h-128 w-128 lg:h-128 lg:w-128 rounded-full"
   />
 </div>
 
