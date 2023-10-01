0d79e87af81ee37d876db4a3e6a47e392b2b13ae
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-10-01 14:58:43 +0100

    added the publications

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Education from "./components/Education"
 import Honours from "./components/Honours"
 import Work from "./components/Work"
 import Membership from "./components/Membership"
+import Publication from "./components/Publication"
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
     <Honours/>
     <Work/>
     <Membership/>
+    <Publication/>
      </div>
     </div>
     </>
--- src/components/Publication.jsx
+++ src/components/Publication.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import Anthropometrics1 from '../assets/ANTHROPOMETRICS1.pdf'
-import Anthropometrics2 from '../assets/ANTHROPOMETRICS2.pdf'
-import Anthropometrics3 from '../assets/ANTHROPOMETRICS3.pdf'
-import Anthropometrics4 from '../assets/ANTHROPOMETRICS4.pdf'
-import Anthropometrics5 from '../assets/ANTHROPOMETRICS5.pdf'
-import Anthropometrics6 from '../assets/ANTHROPOMETRICS6.pdf'
+import React from 'react';
+import { files } from './Publicationdata';
 
 function Publication() {
   return (
-    <div className=" publications my-10  py-5 px-3 ">
-
+    <div className="publications my-10 py-5 px-3">
+      <h1 className='text-4xl text-center mb-4 font-bold'>Publications</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4">
+        {files.map((file) => (
+          <a className="antro-1 flex flex-col items-center justify-center text-center p-4 border border-gray-300 rounded-lg hover:bg-gray-100" href={file.path} download="" key={file.name}>
+            <div className="img">
+              <img
+                src={file.link}
+                alt=""
+                className="w-full max-w-full h-auto max-h-32 "
+              />
+            </div>
+            <div className="text mt-2">
+              <p className="">
+                publication:{' '}
+                <span className='text-grey-700 text-sm font-bold'>{file.name}</span>
+              </p>
+              <p>
+                Author: <span className="text-gray-700 text-sm font-bold">{file.author}</span>
+              </p>
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Publication
+export default Publication;
