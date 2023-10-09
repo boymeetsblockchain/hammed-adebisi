0f0d5c83f753cdf98433fd256acc83dda13b8cca
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-10-09 07:45:07 +0100

    justified the text

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,7 +6,7 @@ function Education() {
       <h1 className="text-center font-bold text-lg md:text-4xl">Educational Background</h1>
       <div className="edu-box flex">
         <div className="higher shadow-lg p-6 rounded-md my-3  bg-white w-full">
-          <p className='le'>
+          <p className='text-justify'>
             Dr. Hammed received his Bachelor of Physiotherapy from the Faculty of Medicine, Bayero University Kano, Kano, Nigeria. He also has postgraduate professional degrees (MSc and PhD) in exercise physiology with distinctions from the University of Benin, Benin City, Nigeria. Dr. Hammed's career embodies not only breadth and depth of experience and knowledge but also an international component, including registration and certification by the Health and Care Professionals Council-Physiotherapist (HCPC) to join the UK workforce.
           </p>
         </div>
