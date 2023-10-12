20d5f3c2eaa1ad4b2fc9023915cfea89d742a9bc
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-10-12 10:36:06 +0100

    changed the spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,11 @@ function Hero() {
   return (
     <div className="hero flex flex-col-reverse md:flex-row w-full gap-2  my-10 items-center">
       <div className="info flex flex-col justify-center md:w-1/2 md:mx-[100px] space-y-3 text-center md:text-left">
-        <h1 className='text-3xl  md:text-4xl text-neutral-800 font-bold'>
+        <h1 className='text-2xl  md:text-4xl text-neutral-800 font-bold'>
           Hammed I. Adebisi, PhD
         </h1>
         <h2 className='text-xl  font-semibold'>
-          Neurological Physiotherapist, Exercise Physiologist,Public Health Specialist and Clinical Researcher
+          Neurological Physiotherapist, Exercise Physiologist, {' '}Public Health Specialist and Clinical Researcher
         </h2>
         <p className="text-gray-700   md:px-0 px-3 leading-6 text-justify">
         Dr. Hammed is a seasoned healthcare professional with a diverse background encompassing medical rehabilitation, exercise physiology, public health, and clinical research. With a passion for optimizing human wellness, his career has been marked by a commitment to improving the lives of human through a multifaceted approach. Dr. Hammed has honed his expertise with a solid foundation in the science of human movement through his educational background (PhD in exercise physiology). He is also a dedicated public health specialist with a profound passion for improving community health. Furthermore, Dr. Hammed’s role as a clinical researcher drives innovation and evidence-based practice within the field of medicine. He is armed with a profound passion for advancing healthcare through rigorous scientific investigations and has published over twenty-seven researches in reputable journals.
