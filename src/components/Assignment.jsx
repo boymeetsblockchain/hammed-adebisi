9422079378f5c534db09bd48061d6af1f28c63ee
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-10-15 21:30:02 +0100

    spell check

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -7,7 +7,7 @@ function Assignment() {
      <p className="text-gray-700  text-justify">
      In 2017, Dr. Hammed was recommended by the Nigeria’s Honorable Minister of Communication for the 2017 Hajj operation. 
      The Honorable Minister recommended him for the operation of the Medical Team of the National Hajj Commission of Nigeria, in carrying out a smooth Hajj exercise in Nigeria and Saudi Arabia for the 2017 Hajj.
-      Furthermore, during the Covid-19 pandemic, Dr. Hammed was a member of the Medical team at the isolation center for about six months in Edo State, Nigeria. Also, as an exercise physiologist and physiotherapist, Dr. Hammed was a member of the medical team at the Edo 2020 National Sports Festival which took place in March 2021 because of the Covid-19 pandemic. He was the team lead that cover men and women football matches at the University of Benin Sports Complex, Benin City, Edo State.
+      Furthermore, during the Covid-19 pandemic, Dr. Hammed was a member of the medical team at the isolation center for about six months in Edo State, Nigeria. Also, as an exercise physiologist and physiotherapist, Dr. Hammed was a member of the medical team at the Edo 2020 National Sports Festival which took place in March 2021 because of the Covid-19 pandemic. He was the team lead that cover men and women football matches at the University of Benin Sports Complex, Benin City, Edo State.
      </p>
     </div>
   );
